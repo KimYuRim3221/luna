46067811abb9372b091b5551243d2aa4cc93981d
KimYuRim3221 <150370761+KimYuRim3221@users.noreply.github.com>
2023-11-24 12:26:58 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,26 +35,6 @@ $(function(){
         });
     });
 
-// 메인섹션3이미지전환효과
-$(function(){
-    $("#button1").click(function(){
-
-            $(".img1").fadeIn().siblings().fadeOut();
-    });	
-    $("#button2").click(function(){
-
-            $(".img2").fadeIn().siblings().fadeOut();
-    });	
-    $("#button3").click(function(){
-
-            $(".img3").fadeIn().siblings().fadeOut();
-    });	
-    $("#button4").click(function(){
-
-            $(".img4").fadeIn().siblings().fadeOut();
-    });	
-});
-
 
 
         
